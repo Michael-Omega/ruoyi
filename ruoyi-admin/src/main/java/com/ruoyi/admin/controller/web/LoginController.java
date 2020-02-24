5d514acd5ff0e2ceb5bcce72b86c935b54c1e21d
wx <wuxiong1028@foxmail.com>
2020-02-24 10:18:44 +0800

    项目目录重建1.1

---
+++ ruoyi-admin/src/main/java/com/ruoyi/admin/controller/web/LoginController.java
@@ -0,0 +1,39 @@
+package com.ruoyi.admin.controller.web;
+
+
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.utils.ServletUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/web")
+public class LoginController extends BaseController {
+
+    @RequestMapping("/login")
+    public String Login(HttpServletResponse response, HttpServletRequest request){
+        // 如果是Ajax请求，返回Json字符串。
+        if (ServletUtils.isAjaxRequest(request))
+        {
+            return ServletUtils.renderString(response, "{\"code\":\"1\",\"msg\":\"未登录或登录超时。请重新登录\"}");
+        }
+        return "web/index";
+    }
+
+    @RequestMapping("/handleLogin")
+    @ResponseBody
+    public AjaxResult login(@RequestParam(value = "username",required = true) String username,@RequestParam(value = "password",required = true) String password){
+        Map data = new HashMap();
+        data.put("username",username);
+        data.put("password",password);
+        return AjaxResult.success(data);
+    }
+}
--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/ShiroConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/ShiroConfig.java
@@ -250,11 +250,11 @@ public class ShiroConfig
         filterChainDefinitionMap.put("/js/**", "anon");
         filterChainDefinitionMap.put("/ruoyi/**", "anon");
         filterChainDefinitionMap.put("/druid/**", "anon");
-        filterChainDefinitionMap.put("/captcha/captchaImage**", "anon");
+        filterChainDefinitionMap.put("/admin/captcha/captchaImage**", "anon");
         // 退出 logout地址，shiro去清除session
-        filterChainDefinitionMap.put("/logout", "logout");
+        filterChainDefinitionMap.put("/admin/logout", "logout");
         // 不需要拦截的访问
-        filterChainDefinitionMap.put("/login", "anon,captchaValidate");
+        filterChainDefinitionMap.put("/admin/login", "anon,captchaValidate");
         // 系统权限列表
         // filterChainDefinitionMap.putAll(SpringUtils.getBean(IMenuService.class).selectPermsAll());
 
